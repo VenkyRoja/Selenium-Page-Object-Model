e699baf5666b89c6c1857a60b4ee634abc00d62f
venkyrojavishnu <venkyrojavishnu@localhost>
2021-05-05 12:11:29 -0400

    testing a fail scenario

--- src/main/java/crm/pages/TasksPage.java
+++ src/main/java/crm/pages/TasksPage.java
@@ -10,7 +10,7 @@ public class TasksPage extends TestBase {
 
 	//--------Page Factory-------------Object Repository------------
 	
-	@FindBy(xpath="//div[contains(@class, 'ui header')]")
+	@FindBy(xpath="//div[contains(@class, 'ui headerxxxxxxx')]")
 	WebElement tasksLabel;	
 	
 	//------------1-------------Constructor---------
