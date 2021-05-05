147cde099660e036fea55c13e47ac8dd4fd47bee
venkyrojavishnu <venkyrojavishnu@localhost>
2021-05-05 13:27:55 -0400

    added fail scenario in TasksPage

--- src/main/java/crm/pages/TasksPage.java
+++ src/main/java/crm/pages/TasksPage.java
@@ -10,7 +10,7 @@ public class TasksPage extends TestBase {
 
 	//--------Page Factory-------------Object Repository------------
 	
-	@FindBy(xpath="//div[contains(@class, 'ui headeryyyyyyyyyyyyyyyyyyyyyy')]")
+	@FindBy(xpath="//div[contains(@class, 'ui headeryyy')]")
 	WebElement tasksLabel;	
 	
 	//------------1-------------Constructor---------
